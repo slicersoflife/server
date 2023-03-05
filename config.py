cd2a1eff342edec601d86b4884a18b6462c39152
Saptarshi Bhattacherya <bhat.bab@gmail.com>
2023-03-04 20:56:01 -0700

    chore: update default PORT value

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class BaseConfig:
     """Base configuration."""
 
     DATABASE_URL = os.getenv("DATABASE_URL")
-    PORT = os.getenv("PORT", 8080)
+    PORT = os.getenv("PORT", "8080")
     SECRET_KEY = os.getenv("SECRET_KEY", "my_precious")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CORS_HEADERS = "Content-Type"
