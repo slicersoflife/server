1e3e6e31f226ccb720a2ddd7377b40d6e6d86a7b
Kushal <kushaldeepm@gmail.com>
2023-03-23 18:42:28 -0700

    testing pre commit hook

--- app/auth/models.py
+++ app/auth/models.py
@@ -4,6 +4,7 @@ from sqlalchemy.sql.sqltypes import INT
 
 from app.extensions import db
 
+
 class User(db.Model):
     __tablename__ = "users"
 
@@ -12,17 +13,21 @@ class User(db.Model):
     username = Column(String(20), nullable=False, unique=True)
     phone = Column(String(20), nullable=False, unique=True)
     profile_picture_url = Column(String(255), nullable=True)
-    friend_requests_sent = relationship('FriendRequest', foreign_keys='FriendRequest.from_user_id')
-    friend_requests_received = relationship('FriendRequest', foreign_keys='FriendRequest.to_user_id')
+    friend_requests_sent = relationship(
+        "FriendRequest", foreign_keys="FriendRequest.from_user_id"
+    )
+    friend_requests_received = relationship(
+        "FriendRequest", foreign_keys="FriendRequest.to_user_id"
+    )
 
     def __repr__(self):
         return f"User<{self.id} | {self.username}>"
 
+
 class FriendRequest(db.model):
-    __tablename__ = 'friend_requests'
+    __tablename__ = "friend_requests"
 
     id_requests = Column(UUID, primary_key=True)
-    from_user_id = Column(String, ForeignKey('users.id'), nullable=False)
-    to_user_id = Column(String, ForeignKey('users.id'), nullable=False)
-    status = Column(Enum('accepted', 'rejected', 'pending'), nullable=False)
-
+    from_user_id = Column(String, ForeignKey("users.id"), nullable=False)
+    to_user_id = Column(String, ForeignKey("users.id"), nullable=False)
+    status = Column(Enum("accepted", "rejected", "pending"), nullable=False)
