88f56938bc8ad4b932497786f3863771155d6df9
Kushal <kushaldeepm@gmail.com>
2023-03-23 18:20:16 -0700

    db table config edits and friendslist

--- db_scripts/dbinit.sql
+++ db_scripts/dbinit.sql
@@ -3,19 +3,19 @@ CREATE DATABASE IF NOT EXISTS slice_users;
 USE slice-users;
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT,
-    phonenumber VARCHAR(20) NOT NULL,
+    id uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    phone VARCHAR(20) NOT NULL,
     username VARCHAR(20) NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id)
+    display_name VARCHAR(50) NOT NULL,
+    profile_picture_url VARCHAR(255) NULL,
+    UNIQUE INDEX users_display_name (display_name ASC)
 );
 
 CREATE TABLE IF NOT EXISTS friend_requests (
-    id INT NOT NULL AUTO_INCREMENT,
+    id_requests uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
     from_user_id INT NOT NULL,
     to_user_id INT NOT NULL,
     status ENUM("accepted", "rejected", "pending") NOT NULL,
-    PRIMARY KEY (id),
     FOREIGN KEY (from_user_id) REFERENCES users(id),
     FOREIGN KEY (to_user_id) REFERENCES users(id)
 );
