f4fcc94c1029d804992a6fcceaf2ae583648b224
Kushal <kushal.mujral@miovision.com>
2023-05-21 13:46:26 -0400

    testing endpoints with postman and updating

--- app/config.py
+++ app/config.py
@@ -8,6 +8,8 @@ class BaseConfig:
     SECRET_KEY = os.getenv("SECRET_KEY", "my_precious")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_ENGINE_OPTIONS = {"supports_statement_cache": False}
+
     CORS_HEADERS = "Content-Type"
 
     VERIFICATION_CODE_LENGTH = 6
--- app/friends/routes.py
+++ app/friends/routes.py
@@ -1,5 +1,6 @@
 from flask import request, jsonify, Blueprint, current_app
 from sqlalchemy import select
+import uuid
 
 from app.extensions import db
 from .models import Friend, FriendRequest
@@ -10,22 +11,22 @@ def add_routes(bp: Blueprint):
     def friend_request():
         # add POST route to save a new entry in the friend request table, taking in the user id of the person who sent the request and the user id of the person who received the request
         post_data = request.get_json()
-        if to_user_id not in post_data or from_user_id not in post_data:
-            response_object = {
-                "status": "fail",
-                "message": "Invalid request",
-            }
-            return jsonify(response_object), 401
         try:
             from_user_id = post_data.get("from_user_id")
             to_user_id = post_data.get("to_user_id")
-            friend_request = FriendRequest(from_user_id, to_user_id)
+
+            friend_request = FriendRequest(
+                id=uuid(),
+                from_user_id=post_data.get("from_user_id"),
+                to_user_id=post_data.get("to_user_id"),
+            )
             db.session.add(friend_request)
             db.session.commit()
             response_object = {
                 "status": "success",
                 "message": "Friend request sent",
             }
+            db.session.close()
             return jsonify(response_object), 200
         except Exception as exception:
             print(exception)
@@ -44,7 +45,7 @@ def add_routes(bp: Blueprint):
             ).first()
             db.session.delete(friend_request)
             db.session.commit()
-            friend = Friend(from_user_id, to_user_id)
+            friend = Friend(id=uuid(), from_user_id=from_user_id, to_user_id=to_user_id)
             db.session.add(friend)
             db.session.commit()
             response_object = {
